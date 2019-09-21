9192b97ba3e477156a523124fdee93ad921b6eba
SebastianS85 <smuk.sebastian@gmx.de>
2019-09-21 14:50:02 +0200

    fix delete

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl + '/?' + $.param({
-        taskId: taskId
+        Id: taskId
       }),
       method: 'DELETE',
       success: function() {
